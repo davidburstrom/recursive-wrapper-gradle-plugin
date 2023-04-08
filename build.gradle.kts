4835d33bc02d7785e2f67edd3290a2c746a6a3f5
David Burström <david.burstrom@gmail.com>
2023-04-08 13:14:50 +0200

    Bump Gradle Plugin Publish plugin to 1.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.diffplug.spotless") version "6.18.0"
     id("com.github.ben-manes.versions") version "0.46.0"
-    id("com.gradle.plugin-publish") version "1.1.0"
+    id("com.gradle.plugin-publish") version "1.2.0"
     id("io.github.davidburstrom.version-compatibility") version "0.5.0"
     id("net.ltgt.errorprone") version "3.0.1"
     id("org.gradle.signing")
