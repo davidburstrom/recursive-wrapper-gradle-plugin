0125c94ce2207d810b370af2d848331212f47ba9
David Burström <david.burstrom@gmail.com>
2023-09-02 10:09:41 +0200

    Bump Spotless to 6.21.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.diffplug.spotless") version "6.20.0"
+    id("com.diffplug.spotless") version "6.21.0"
     id("com.github.ben-manes.versions") version "0.47.0"
     id("com.gradle.plugin-publish") version "1.2.0"
     id("io.github.davidburstrom.version-compatibility") version "0.5.0"
